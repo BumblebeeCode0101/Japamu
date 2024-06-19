e97e9da8b102144b3d37190cd48387767940bde1
BumblebeeCode0101 <janik1113@icloud.com>
2024-06-19 17:56:03 +0200

    Added advanced password validation

--- register.php
+++ register.php
@@ -21,8 +21,20 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     
     if (empty($password)) {
         $errors[] = "Password is required.";
+    } elseif (strlen($password) < 8) {
+        $errors[] = "Password must be at least 8 characters long.";
+    } elseif (!preg_match('/[A-Z]/', $password)) {
+        $errors[] = "Password must contain at least one uppercase letter.";
+    } elseif (!preg_match('/[a-z]/', $password)) {
+        $errors[] = "Password must contain at least one lowercase letter.";
+    } elseif (!preg_match('/\d/', $password)) {
+        $errors[] = "Password must contain at least one digit.";
+    } elseif (!preg_match('/[^A-Za-z0-9]/', $password)) {
+        $errors[] = "Password must contain at least one special character.";
+    } elseif (!preg_match("/[!@#$%^&*()_+\-=\[\]{};':\\|,.<>\/?]+/", $password)) {
+        $errors[] = "Password must contain at least one special character (!@#$%^&*()_+).";
     }
-    
+
     if (empty($tag)) {
         $errors[] = "Tag is required.";
     } elseif (strpos($tag, ' ') !== false) {
@@ -80,6 +92,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             var nameValue = nameElement.value.trim();
             var generatedTag = generateTag(nameValue); 
             tagElement.value = generatedTag; 
+        });
+
+        passwordElement.addEventListener('input', function() {
+
         });
 
         function generateTag(name) {
